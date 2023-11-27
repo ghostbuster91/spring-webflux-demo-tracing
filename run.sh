d91b8434141d1c0b8f15b452e465e5cbafbee7a8
ghostbuster91 <ghostbuster91@users.noreply.github.com>
2023-11-27 10:37:39 +0100

    Add missing async flag for agent

--- run.sh
+++ run.sh
@@ -3,9 +3,10 @@
 wget -O dd-java-agent.jar https://dtdg.co/latest-java-tracer
 
 java -javaagent:dd-java-agent.jar \
-  -Ddd.logs.injection=true \
-  -Ddd.service=http-example \
-  -Ddd.env=test \
-  -Ddd.trace.enabled=true \
-  -Ddd.trace.propagation.style="b3multi" \
-  -jar build/libs/*.jar
+	-Ddd.logs.injection=true \
+	-Ddd.service=http-example \
+	-Ddd.env=test \
+	-Ddd.trace.enabled=true \
+	-Ddd.trace.annotation.async=true \
+	-Ddd.trace.propagation.style="b3multi" \
+	-jar build/libs/*.jar
